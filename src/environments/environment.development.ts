0eb7fba7152fc7301f0f4e34cbef3ef2c0d30ebb
mcarbajalc2 <42019150+mcarbajalc2@users.noreply.github.com>
2024-09-20 20:32:36 -0500

    feat: modulos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,8 +3,8 @@ export const environment = {
 
   production: false,
 
-  // API_BASE: 'http://143.198.161.217/api/test/',
-  API_BASE: 'http://localhost:8000/api/test/',
+  API_BASE: 'http://143.198.161.217/api/test/',
+  // API_BASE: 'http://localhost:8000/api/test/',
   
   parametro: 'parametro',
 };
